2907aefbc2728a34affe8a36177a328d69cfbd6d
koba-ppp <kobayashi.teppei.0923@gmail.com>
2021-03-03 15:33:10 +0900

    fukushima

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>神奈川大学電気電子情報工学科所属</p>
+        <p>神奈川大学電気電子情報工学科所属、福島県出身</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
